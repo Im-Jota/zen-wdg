543c6ffc21f85a725036f719f2d25e8c73e93073
Im-Jota <joseluisromeroarce12@gmail.com>
2025-07-25 15:20:27 -0500

    chore: export marked and todo widget

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,10 +1,14 @@
 import type { defineCustomElement, DefineCustomElementReturn } from 'vue';
 
 import ClockWidget from './widgets/z-clock-widget.vue';
-import SearchWidget from './widgets/z-search-widget.vue'
+import SearchWidget from './widgets/z-search-widget.vue';
+import TodoWidget from './widgets/z-todo-widget.vue';
+import MarkedWidget from './widgets/z-marked-widget.vue';
 
 export const ZClockWidget = defineCustomElement(ClockWidget);
 export const ZSearchWidget = defineCustomElement(SearchWidget);
+export const ZTodoWidget = defineCustomElement(TodoWidget);
+export const ZMarkedWidget = defineCustomElement(MarkedWidget);
 
 export function registerAllWidgets() {
   if (!customElements.get('z-clock-widget')) {
@@ -13,5 +17,13 @@ export function registerAllWidgets() {
 
   if (!customElements.get('z-search-widget')) {
     customElements.define('z-search-widget', ZSearchWidget);
+  }
+
+  if (!customElements.get('z-todo-widget')) {
+    customElements.define('z-todo-widget', ZTodoWidget);
+  }
+
+  if (!customElements.get('z-marked-widget')) {
+    customElements.define('z-marked-widget', ZMarkedWidget);
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,31 @@
 import { defineCustomElement } from 'vue'
 import ZClockWidget from './widgets/z-clock-widget.vue'
+import ZSearchWidget from './widgets/z-search-widget.vue'
+import ZTodoWidget from './widgets/z-todo-widget.vue'
+import ZMarkedWidget from './widgets/z-marked-widget.vue'
 
 // Crear el custom element
 const ZClockElement = defineCustomElement(ZClockWidget)
+const ZSearchElement = defineCustomElement(ZSearchWidget)
+const ZTodoElement = defineCustomElement(ZTodoWidget)
+const ZMarkedElement = defineCustomElement(ZMarkedWidget)
 
 // Registrar custom element (evitar doble registro)
 if (!customElements.get('z-clock-widget')) {
   customElements.define('z-clock-widget', ZClockElement)
 }
 
+if (!customElements.get('z-search-widget')) {
+  customElements.define('z-search-widget', ZSearchElement)
+}
+
+if (!customElements.get('z-todo-widget')) {
+  customElements.define('z-todo-widget', ZTodoElement)
+}
+
+if (!customElements.get('z-marked-widget')) {
+  customElements.define('z-marked-widget', ZMarkedElement)
+}
+
 // Exportar el componente para uso tradicional Vue
-export { ZClockWidget }
+export { ZClockWidget, ZSearchWidget, ZTodoWidget, ZMarkedWidget }
