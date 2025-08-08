f7607071bd403ab0c158d62f90b2bf547fcd9041
Im-Jota <joseluisromeroarce12@gmail.com>
2025-08-08 10:16:37 -0500

    feat: add modal component

---
+++ src/components/UI/Modal.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="container" :class="{ 'show': show }" @click="toggleModal"></div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            show: false,
+        }
+    },
+
+    methods: {
+        toggleModal() {
+            this.show = !this.show
+        }
+    }
+}
+</script>
+
+<style scoped>
+.container {
+    position: fixed;
+    top: 0;
+    right: 0;
+    width: 100%;
+    height: 100vh;
+    overflow: hidden;
+    box-sizing: border-box;
+    font-size: 14px;
+    background-color: rgba(0, 0, 0, 0);
+    opacity: 0;
+    transition: background-color 0.3s ease, opacity 0.3s ease;
+    visibility: hidden;
+    z-index: 5;
+}
+
+.container.show {
+  visibility: visible;
+  opacity: 1;
+  background-color: rgba(10, 10, 10, .4);
+}
+</style>
