73d5a018cc2cef91e41c298056f09ba0808015cb
Im-Jota <joseluisromeroarce12@gmail.com>
2025-08-08 10:18:02 -0500

    feat: add multiple search engines

--- src/widgets/z-search-widget.vue
+++ src/widgets/z-search-widget.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="container-search">
     <div class="search">
-      <img v-if="logo" :src="logo" />
+      <div>
+        <Dropdown :options="engines" v-model="engine" />
+      </div>
       <Input placeholder="Search something" full class="bg-glass" v-model="text" @enter-pressed="search()" />
       <Button icon="material-symbols-light:search-rounded" size="25px" flat class="text-color" @click="search()" />
     </div>
@@ -11,12 +13,36 @@
 <script>
 import Input from '../components/UI/Input.vue';
 import Button from '../components/UI/Button.vue';
+import Dropdown from '../components/UI/Dropdown.vue';
 export default {
   name: 'ZSearchWidget',
 
   data() {
     return {
       text: '',
+      engines: [
+        {
+          id: 1,
+          logo: 'https://s1.qwant.com/thumbr/474x474/b/c/eae6b919559217567ef067dec4aa1e425d560667e5b8e51333bea1b60f4de7/OIP.G3kcQIn5w4kJtUilFbWaigHaHa.jpg?u=https%3A%2F%2Fthf.bing.com%2Fth%2Fid%2FOIP.G3kcQIn5w4kJtUilFbWaigHaHa%3Fr%3D0%26cb%3Dthfc1%26pid%3DApi&q=0&b=1&p=0&a=0',
+          label: 'Qwant',
+          search: `https://www.qwant.com/?l=es&q=`
+        },
+        {
+          id: 2,
+          logo: 'https://astian.org/wp-content/uploads/2025/06/favicon-1.png',
+          label: 'MidoriGO',
+          search: 'https://astiango.co/?q='
+        }
+      ],
+      engine: {
+        id: 1,
+        logo: 'https://s1.qwant.com/thumbr/474x474/b/c/eae6b919559217567ef067dec4aa1e425d560667e5b8e51333bea1b60f4de7/OIP.G3kcQIn5w4kJtUilFbWaigHaHa.jpg?u=https%3A%2F%2Fthf.bing.com%2Fth%2Fid%2FOIP.G3kcQIn5w4kJtUilFbWaigHaHa%3Fr%3D0%26cb%3Dthfc1%26pid%3DApi&q=0&b=1&p=0&a=0',
+        label: 'Qwant',
+        search: `https://www.qwant.com/?l=es&q=`
+      },
+      urls: {
+        url: 'https://www.qwant.com/?l=es&q=facebook+login&t=web',
+      }
     }
   },
 
@@ -34,17 +60,18 @@ export default {
 
   components: {
     Input,
-    Button
+    Button,
+    Dropdown
   },
 
   methods: {
     search() {
-      const query = encodeURIComponent(this.text);
-      const url = `https://astiango.co/?q=${query}`;
-      if(this.searchTarget == 'New Tab') {
-        chrome.tabs.create({ url: url});
+      console.log(this.engine.search)
+      const url = `${this.engine.search}${this.text}`
+      if (this.searchTarget == 'New Tab') {
+        chrome.tabs.create({ url: url });
         this.text = '';
-      } else if(this.searchTarget == 'Self Tab') {
+      } else if (this.searchTarget == 'Self Tab') {
         chrome.tabs.update({ url: url });
       }
     }
@@ -57,11 +84,11 @@ export default {
   width: 100%;
   backdrop-filter: blur(10px) saturate(180%);
   -webkit-backdrop-filter: blur(10px) saturate(180%);
-  background-color: rgba(20, 20, 20, 0.1) !important;
+  background-color: rgba(20, 20, 20, 0.1);
   border: 1px solid rgba(255, 255, 255, 0.3);
   color: white;
   box-shadow: 0 8px 32px 0 rgba(31, 38, 135, 0.2);
-  padding: .5rem;
+  padding: .4rem;
   border-radius: .6rem;
 }
 
@@ -70,7 +97,7 @@ export default {
   background-color: rgba(10, 10, 10, .6);
   display: flex;
   align-items: center;
-  padding: .2rem 1rem;
+  padding: .3rem .8rem;
   border-radius: .5rem;
   box-shadow: 0 0 .4rem var(--border-color);
 }
